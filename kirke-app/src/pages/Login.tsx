3a09c5fbb6b5673edc3988be9073081f3ada561a
Michael <hovgaard1990@gmail.com>
2023-03-21 13:22:51 +0100

    moved singinwithemail to own component for semantisk code
    Reset password er addet.
    Sign in with mail er rykket til et component for sig selv.
    Ændrede i interfacen for at give reset tekster.

--- kirke-app/src/pages/Login.tsx
+++ kirke-app/src/pages/Login.tsx
@@ -1,74 +1,22 @@
 import React, { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 import { useNavigate } from "react-router-dom";
-import style from "../style/warning.module.css";
+
 import { useLanguage } from "../components/LanguageContext";
-import LanguageSwitcher from "../components/LanguageSwitcher";
+import SignIn from "../components/signInWithEmail";
+import ResetPassword from "../components/ResetPassword";
 
 export interface ILoginPageProps {}
 
 const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
-  const auth = getAuth();
-  const navigate = useNavigate();
-  const [authing, setAuthing] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
   const { locale } = useLanguage();
   const home = locale.login;
-
-  const signInWithEmail = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setAuthing(true);
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((response) => {
-        console.log(response.user.uid);
-        localStorage.setItem("userEmail", email);
-        navigate("/");
-      })
-      .catch((error) => {
-        console.log(error);
-        setShowAlert(true);
-        setAuthing(false);
-      });
-  };
-
-  const handleAlertClose = () => {
-    setShowAlert(false);
-  };
 
   return (
     <div>
       <p>{home.title}</p>
-      {showAlert && (
-        <div className={style.alert}>
-          <span className={style.closebtn} onClick={handleAlertClose}>
-            &times;
-          </span>
-          <strong>{locale.error.loginFailed}</strong>
-        </div>
-      )}
-
-      <form onSubmit={signInWithEmail}>
-        <input
-          type="email"
-          placeholder={home.email}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder={home.password}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" disabled={authing}>
-          {home.submit}
-        </button>
-      </form>
-      <br />
-      <LanguageSwitcher></LanguageSwitcher>
+      <SignIn />
+      <ResetPassword />
     </div>
   );
 };
