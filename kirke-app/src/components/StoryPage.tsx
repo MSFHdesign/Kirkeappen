638d74f17f2091db38ad57714417aa5616c25522
Michael <hovgaard1990@gmail.com>
2023-03-22 08:51:24 +0100

    add interface as export

---
+++ kirke-app/src/components/StoryPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+const StoryPage = (props: Props) => {
+  return <div>StoryPage</div>;
+};
---
+++ kirke-app/src/models/iStory.ts
@@ -0,0 +1,5 @@
+export interface iStory {
+  id: string;
+  title: string;
+  content: string;
+}
