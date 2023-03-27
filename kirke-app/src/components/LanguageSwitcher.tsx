5ff8f027008778ffd96722fb8494a2fad26ac5a8
MagnusKingoDanielsen <magnus@metteogivar.dk>
2023-03-27 12:56:30 +0200

    styled and changed login page

--- kirke-app/src/components/LanguageSwitcher.tsx
+++ kirke-app/src/components/LanguageSwitcher.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import { useLanguage } from "./LanguageContext";
+import dk from "../img/dk.png";
+import uk from "../img/uk.png";
 
 const LanguageSwitcher: React.FC = () => {
   const { toggleLocale } = useLanguage();
-  const [buttonText, setButtonText] = useState("SWITCH TO ENGLISH");
+  const [languageFlag, setLanguageFlag] = useState(uk);
 
   const handleClick = () => {
-    if (buttonText === "SWITCH TO ENGLISH") {
-      setButtonText("SKIFT TIL DANSK");
+    if (languageFlag === uk) {
+      setLanguageFlag(dk);
       toggleLocale();
     } else {
-      setButtonText("SWITCH TO ENGLISH");
+      setLanguageFlag(uk);
       toggleLocale();
     }
   };
 
   return (
     <div>
-      <button onClick={handleClick}>{buttonText}</button>
+      <img
+        src={languageFlag}
+        alt="languageFlag"
+        width={100}
+        height={50}
+        onClick={handleClick}
+      />
     </div>
   );
 };
