4792bb99a700f6a275ca806e04e681ccdba669c6
Michael <hovgaard1990@gmail.com>
2023-05-01 15:29:54 +0200

    Update SignOut.tsx
    Added comments

--- kirke-app/src/components/SignOut.tsx
+++ kirke-app/src/components/SignOut.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { getAuth, signOut } from "firebase/auth";
 
+// this component is a signout component, that call a funktions from auth and logging the user out.
+// it also clear the local storage from any values. So any uninspected snoopers can get inside.
+
 interface SignOutProps {}
 
 const SignOut: React.FC<SignOutProps> = () => {
